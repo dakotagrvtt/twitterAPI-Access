72391811a38fa6adf2f0d93fd185ac6e974e4049
Dakota Gravitt <dakotagrvtt50@gmail.com>
2020-01-30 17:51:47 -0600

    Included boilerplate key constants code

---
+++ twitter_keys.py
@@ -0,0 +1,15 @@
+# Replace constants with your own dev keys for API access
+
+import tweepy
+
+API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+API_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+ACCESS_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+ACCESS_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+'''def setupAPI():
+    auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    return tweepy.API(auth)
+'''
